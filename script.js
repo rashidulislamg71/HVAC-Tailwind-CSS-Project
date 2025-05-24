c07eeaaaa6a0ab659020d42dd016c6b241e66152
rashidulislamg71 <jrashidulislam0103@gmail.com>
2025-05-24 13:08:49 +0600

    HVAC First commit

---
+++ script.js
@@ -0,0 +1,14 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      fontFamily: {
+        opensans: ['"Open Sans"', "sans-serif"],
+        portlligat: ['"Port Lligat Slab"', "serif"],
+      },
+      boxShadow: {
+        customShadow: ["0px 2px 6px rgba(110, 108, 108, 0.774)"],
+      },
+     
+    },
+  },
+};
